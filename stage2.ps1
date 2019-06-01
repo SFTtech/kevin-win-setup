fcf1eea1170f3b20d8e0df96bfd29ea33528cf5f
simonsan <simon@systemli.org>
2019-06-01 16:02:45 +0200

    VS Build tools now downloading and installing

--- stage2.ps1
+++ stage2.ps1
@@ -12,15 +12,19 @@
 # that will Download automatically the newest Release-Version
 # Search for #ADD NEW SOFTWARE LINK GENERATION HERE and add a new case for the Link generation
 $deps = @(
-#    [PSCustomObject]@{Name = "cmake"; isGit=$true; isZip=$true; isInstaller=$false; LatestRelease =""; RepoName = "Kitware/CMake"; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
-#    [PSCustomObject]@{Name = "mingit"; isGit=$true; isZip=$true; isInstaller=$false; LatestRelease = ""; RepoName = "git-for-windows/git"; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
-#    [PSCustomObject]@{Name = "python3"; isGit=$false; isZip=$false; isInstaller=$true; LatestRelease = ""; RepoName = ""; DownloadLink = "https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe"; HashFile = ""; HomeDir = ""; FileName = ""}
-     [PSCustomObject]@{Name = "vs_buildtools"; isGit=$false; isZip=$false; isInstaller=$true; LatestRelease = ""; RepoName = ""; DownloadLink = "https://download.visualstudio.microsoft.com/download/pr/10413969-2070-40ea-a0ca-30f10ec01d1d/414d8e358a8c44dc56cdac752576b402/vs_buildtools.exe"; HashFile = ""; HomeDir = ""; FileName = ""}
+#    [PSCustomObject]@{Name = "cmake"; isGit=$true; isZip=$true; isInstaller=$false; isConfig=$false; LatestRelease =""; RepoName = "Kitware/CMake"; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
+#    [PSCustomObject]@{Name = "mingit"; isGit=$true; isZip=$true; isInstaller=$false; isConfig=$false; LatestRelease = ""; RepoName = "git-for-windows/git"; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
+#    [PSCustomObject]@{Name = "flex"; isGit=$true; isZip=$true; isInstaller=$false; isConfig=$false; LatestRelease =""; RepoName = "lexxmark/winflexbison"; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
+#    [PSCustomObject]@{Name = "python3"; isGit=$false; isZip=$false; isInstaller=$true; isConfig=$false; LatestRelease = ""; RepoName = ""; DownloadLink = "https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe"; HashFile = ""; HomeDir = ""; FileName = ""}
+    [PSCustomObject]@{Name = "vs_buildtools"; isGit=$false; isZip=$false; isInstaller=$true; isConfig=$false; LatestRelease = ""; RepoName = ""; DownloadLink = "https://download.visualstudio.microsoft.com/download/pr/10413969-2070-40ea-a0ca-30f10ec01d1d/414d8e358a8c44dc56cdac752576b402/vs_buildtools.exe"; HashFile = ""; HomeDir = ""; FileName = ""; ConfigPath=""}
+    [PSCustomObject]@{Name = "vs_buildtools\config"; LinkName="vs_buildtools"; isGit=$false; isZip=$false; isInstaller=$false; isConfig=$true; LatestRelease = ""; RepoName = ""; DownloadLink = "https://raw.githubusercontent.com/simonsan/kevin-win-setup/stage2/vsconfig/build_tools.vsconfig"; HashFile = ""; HomeDir = ""; FileName = ""}
   
    # Preset New Download (Search for #ADD NEW SOFTWARE LINK GENERATION HERE and add a new case)
    # [PSCustomObject]@{Name = ""; isGit=""; RepoName = ""; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
    
 )
+
+
 # Command to install the dependencies from pip
 $pip_command="pip install cython numpy pillow pygments pyreadline Jinja2"
 
@@ -28,7 +32,7 @@ $pip_command="pip install cython numpy pillow pygments pyreadline Jinja2"
 $vcpkg_x64_command="vcpkg install dirent eigen3 fontconfig freetype harfbuzz libepoxy libogg libpng opus opusfile qt5-base qt5-declarative qt5-quickcontrols sdl2 sdl2-image --triplet x64-windows"
 
 # Change to preferred download path
-$dependency_dl_path="$env:HOMEDRIVE\openage-deps\";
+$dependency_dl_path="$env:HOMEDRIVE\openage-dl\";
 
 
 # Flag for DRY RUN -> TODO
@@ -69,28 +73,43 @@ Function GenerateDepFolders{
 # TODO Do not redownload already installed/downloaded deps
 # TODO Find a way to cache actual version number in a environment variable/text file
 Function DownloadDependencies($arr){
+
+     Write-Host "Downloading dependencies ..."
     
      $arr | ForEach-Object {
             # Start-BitsTransfer -Source $_.HashFile -Destination $_.HomeDir -Asynchronous
             $source = "$($_.DownloadLink)"
             $output = "$($_.HomeDir)$($_.FileName)"
 
-            Write-Host "Downloading $($_.Name) ..."
-            [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+            # Don't download if file already there
+            if( !(Test-Path $output)){
+           
+                Write-Host "Downloading $($_.Name) ..."
+                [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
             
-            if(($_.Name) -eq "vs_buildtools"){
+                if(($_.Name) -eq "vs_buildtools"){
 
-                 # Visual Studio 17 CE (advanced options) - Build tools
-                 $job = Invoke-WebRequest -Uri $source  -OutFile $output -Headers @{"method"="GET"; "authority"="download.visualstudio.microsoft.com"; "scheme"="https"; "path"="/download/pr/10413969-2070-40ea-a0ca-30f10ec01d1d/414d8e358a8c44dc56cdac752576b402/vs_buildtools.exe"; "upgrade-insecure-requests"="1"; "dnt"="1"; "user-agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"; "accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"; "referer"="https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=15"; "accept-encoding"="gzip, deflate, br"; "accept-language"="en-US,en;q=0.9"}
-       
-            }else{
-                 # Other dependencies besides VS-Buildtools
-                 $job = Invoke-WebRequest -Uri $source -OutFile $output
+                     # Visual Studio 17 CE (advanced options) - Build tools
+              
+                     $job = Invoke-WebRequest -Uri $source  -OutFile $output -Headers @{"method"="GET"; "authority"="download.visualstudio.microsoft.com"; "scheme"="https"; "path"="/download/pr/10413969-2070-40ea-a0ca-30f10ec01d1d/414d8e358a8c44dc56cdac752576b402/vs_buildtools.exe"; "upgrade-insecure-requests"="1"; "dnt"="1"; "user-agent"="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"; "accept"="text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3"; "referer"="https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=BuildTools&rel=15"; "accept-encoding"="gzip, deflate, br"; "accept-language"="en-US,en;q=0.9"}
+                 
+
+                }else{
+                     # Other dependencies besides VS-Buildtools.exe
+                     # But gets config-files for the vs-installer as well
+                     $job = Invoke-WebRequest -Uri $source -OutFile $output
                                     
-            }
+                }
 
-            While ($job.JobState -eq "Transferring") {
-                     Sleep -Seconds 3
+                While ($job.JobState -eq "Transferring") {
+                         Sleep -Seconds 3
+                }
+
+    
+            }elseif(Test-Path $output){
+           
+              Write-Host "$output already exists! Continuing with already existing file."
+    
             }
 
     }
@@ -100,8 +119,14 @@ Function DownloadDependencies($arr){
 # Function to generate FileName and FilePath from DownloadLink
 Function GenerateFileNames($arr){
 
+    Write-Host "Generating Filenames ..."
+
     $arr | ForEach-Object {
            $_.FileName = "$($_.DownloadLink.SubString($_.DownloadLink.LastIndexOf('/') + 1))"
+          
+           # Debug
+           Write-Host $_.HomeDir
+           Write-Host $_.FileName
     }
 }
 
@@ -109,6 +134,8 @@ Function GenerateFileNames($arr){
 # Inspired by https://gist.github.com/f3l3gy/0e89dde158dde024959e36e915abf6bd
 Function GetLatestVersionLink($arr){
     
+    Write-Host "Get the latest version of Github-Releases ..."
+
     $arr | ForEach-Object {
 
            # Github
@@ -143,20 +170,29 @@ Function GetLatestVersionLink($arr){
                    $arch="-win64-x64"
                    $type=".zip"
            
+               }elseif(($_.Name) -eq "flex"){
+                   #win_flex_bison-2.5.18.zip 
+                   $name = "win_$($_.Name)_bison-"  
+                   $version=$_.LatestRelease.Split("v")[1]
+                   $arch=""
+                   $type=".zip"
+           
+               
                }
-          
 
                $file="$name$version$arch$type"
 
                #ADD NEW SOFTWARE LINK GENERATION HERE
                # >>> <<<
-           
-               Write-Host $_.LatestRelease
+               
+               
+               Write-Host "The latest release of $($_.Name) is $($_.LatestRelease)."
 
                # Debug
                # Write-Host "DownloadLink for $($_.RepoName) is"
+               
                $_.DownloadLink="https://github.com/$($_.RepoName)/releases/download/$($_.LatestRelease)/$($file)"
-
+               
                # Debug
                # Write-Host $_.DownloadLink
 
@@ -167,7 +203,6 @@ Function GetLatestVersionLink($arr){
     }
    
 }
-
 
 # Extract Zip-Files and delete archives afterwards
 # Sets the HomeDir to the extracted new Folder structure
@@ -195,24 +230,57 @@ Function ExtractDependencies($arr){
 
 }
 
+# Connect a downloaded config file to a setup
+Function ConnectConfig($arr){
 
+   Write-Host "Connecting Configs ..."
+    #$cfg_path = ($arr | Where {$_.isConfig -and $_.LinkName})
+    #$cfg_path = "$($cfg_path.HomeDir)$($cfg_path.FileName)"
+    
+   $arr | ForEach-Object {
+          if(($_.isConfig) -eq $true){
+          
+            # LinkName should not be empty
+            if($_.LinkName){
+                
+               $linkname_temp = $_.LinkName
+               $cfg_path_temp = "$($_.HomeDir)$($_.FileName)"
+                
+               $change = ($arr | Where {$($_.Name) -eq $($linkname_temp)})  
+               $change.ConfigPath = $cfg_path_temp
+
+            }
+          }
+    
+    }
+
+ }
+ 
 # Install the dependencies
 # TODO Testing
 Function InstallDependencies($arr){
-    
+
+
+
     $arr | ForEach-Object {
          if(($_.isInstaller) -eq $true){
 
 
            if(($_.Name) -eq "python3"){
+
              # Installer Routine for Python
+             Write-Host "Installing $($_.Name), this can take a longer time. Do not close this window!"
 
              $setup = Start-Process "$($_.HomeDir)$($_.FileName)" -ArgumentList "/s /passive Include_debug=1 Include_dev=1 Include_lib=1 Include_pip=1 PrependPath=1 CompileAll=1 InstallAllUsers=1 TargetDir=$($_.HomeDir)" -Wait
              if ($setup.exitcode -eq 0){
                 write-host "$($_.Name) installed succesfully."
              }
            }elseif(($_.Name) -eq "vs_buildtools"){
-             $setup = Start-Process "$($_.HomeDir)$($_.FileName)" -ArgumentList "/s -q --norestart --noUpdateInstaller --downloadThenInstall --lang en-US --force --config " -Wait
+
+             # Installer Routine for VS Buildtools
+             Write-Host "Installing $($_.Name), this can take a longer time. Do not close this window!"
+             $setup = Start-Process "$($_.HomeDir)$($_.FileName)" -ArgumentList "-p --addProductLang En-us --norestart --noUpdateInstaller --downloadThenInstall --force --config $($_.ConfigPath)" -Wait
+
              if ($setup.exitcode -eq 0){
                 write-host "$($_.Name) installed succesfully."
              }
@@ -225,19 +293,11 @@ Function InstallDependencies($arr){
 
     }
 
-
-   
-
-   
-
-   
-    
-
-
 }
 
 
 
+## Main
 
 # Create Directory for dependency downloads
 GenerateDepFolders -arr $deps -path $dependency_dl_path
@@ -254,14 +314,19 @@ DownloadDependencies $deps
 # Extract Dependencies
 #ExtractDependencies $deps
 
+# Link config files
+ConnectConfig $deps
+
 # Install Dependencies
-#InstallDependencies $deps
+InstallDependencies $deps
 
 # Python should be in PATH from here
 # -> PIP
 # git and cmake still missing in PATH
 
 
+
+# Notes
 
 
 # Set Environment variables
@@ -270,6 +335,8 @@ DownloadDependencies $deps
 # cmake
 # git
 # Python (automatically)
+# PATH: Flex
+# Buildtools: C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools
 
 # Alternative to Invoke-Webrequest
 # Start-BitsTransfer -Source $_.HashFile -Destination $_.HomeDir -Asynchronous
@@ -277,8 +344,6 @@ DownloadDependencies $deps
 
 
 # Version caching in Umgebungsvariable oder ähnlichem
-
-# Installer still do DL
 
 # pip install modules
 
