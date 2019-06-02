ddff5bcc0e796fe24e8680edd3334da8fe02acfe
simonsan <simon@systemli.org>
2019-06-02 21:00:36 +0200

    Started to work in architecture, added some TODO thoughts

--- stage2.ps1
+++ stage2.ps1
@@ -1,5 +1,16 @@
 ﻿# Import BITS for file transfers
-#Import-Module BitsTransfer
+# Import-Module BitsTransfer
+
+# TODO Move out everything which could be an extra configuration of openage building
+# e.g. 32bit MSVC 2019 or 64bit MSVC 2017
+# a bit like a "Desired State Configuration" for dependencies
+# DSC-stable.json should have one big configuration of
+# Array/Hash-Table->$dep
+# Folder-Configs
+# Architecture
+# Compiler version
+# standard/binary paths with BinDir = $dep_dl_path + $binary_name
+
 
 # IMPORTANT NOTE!
 # Need to set the following command manually, to run this script on a standard Win10 machine
@@ -10,8 +21,35 @@
 # Change to preferred download path
 $dependency_dl_path="$env:HOMEDRIVE\openage-dl\";
 
+# 32/64 bit
+# TODO Set flag where version exchange can be possible
+$is64bit=$true
+
+# User hardcoded version file
+# TODO Write importer function
+# $hardcoded_version_file="stable.json"
+
 # Change Build-Directory
 $build_dir="$env:HOMEDRIVE\openage-build\";
+
+# Python-DL
+# TODO How to automate version DL?
+$python_dl="https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe"
+
+# Path vcpkg should be in
+$vcpkg_path="$($dependency_dl_path)vcpkg\"
+
+# cmake commad
+if($is64bit){
+   # $build_arch = "Win64"
+   Write-Host "64bit Flag activated! Everything will be downloaded and build in 64bit/x64 architecture!"
+}else{
+   # $build_arch = "Win32"
+   Write-Host "32bit Flag activated! Everything will be downloaded and build in 32bit/x86 architecture!"
+}
+
+# cmake Visual Studio version
+$build_vs_ver = "Visual Studio 15 2017"
 
 # Dependencies to be downloaded
 # GitHub-Files can be referenced with their RepoName and the Flag "isGit"
@@ -21,25 +59,35 @@ $deps = @(
     [PSCustomObject]@{Name = "cmake"; isGit=$true; isZip=$true; isInstaller=$false; isConfig=$false; LatestRelease =""; RepoName = "Kitware/CMake"; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
     [PSCustomObject]@{Name = "mingit"; isGit=$true; isZip=$true; isInstaller=$false; isConfig=$false; LatestRelease = ""; RepoName = "git-for-windows/git"; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
     [PSCustomObject]@{Name = "flex"; isGit=$true; isZip=$true; isInstaller=$false; isConfig=$false; LatestRelease =""; RepoName = "lexxmark/winflexbison"; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
-    [PSCustomObject]@{Name = "python3"; isGit=$false; isZip=$false; isInstaller=$true; isConfig=$false; LatestRelease = ""; RepoName = ""; DownloadLink = "https://www.python.org/ftp/python/3.7.3/python-3.7.3-amd64.exe"; HashFile = ""; HomeDir = ""; FileName = ""}
+    [PSCustomObject]@{Name = "python3"; isGit=$false; isZip=$false; isInstaller=$true; isConfig=$false; LatestRelease = ""; RepoName = ""; DownloadLink = $python_dl; HashFile = ""; HomeDir = ""; FileName = ""}
     [PSCustomObject]@{Name = "vs_buildtools"; isGit=$false; isZip=$false; isInstaller=$true; isConfig=$false; LatestRelease = ""; RepoName = ""; DownloadLink = "https://download.visualstudio.microsoft.com/download/pr/10413969-2070-40ea-a0ca-30f10ec01d1d/414d8e358a8c44dc56cdac752576b402/vs_buildtools.exe"; HashFile = ""; HomeDir = ""; FileName = ""; ConfigPath=""}
     [PSCustomObject]@{Name = "vs_buildtools\config"; LinkName="vs_buildtools"; isGit=$false; isZip=$false; isInstaller=$false; isConfig=$true; LatestRelease = ""; RepoName = ""; DownloadLink = "https://raw.githubusercontent.com/simonsan/kevin-win-setup/stage2/vsconfig/build_tools.vsconfig"; HashFile = ""; HomeDir = ""; FileName = ""}
 #   [PSCustomObject]@{Name = "nsis"; LinkName=""; isGit=$false; isZip=$false; isInstaller=$false; isConfig=$false; LatestRelease = ""; RepoName = ""; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""}
-  
+#   TODO: Font-Download  
    # Preset New Download (Search for #ADD NEW SOFTWARE LINK GENERATION HERE and add a new case)
    # [PSCustomObject]@{Name = ""; isGit=""; RepoName = ""; DownloadLink = ""; HashFile = ""; HomeDir = ""; FileName = ""} 
 )
 
+# TODO Move $deps to json file
+# Always import from there
+#$deps | ConvertTo-Json -depth 100 | Out-File "$($dependency_dl_path)latest2.json"
+
+# From JSON 
+#$deps_import = (Get-Content -Raw -Path "$($dependency_dl_path)latest.json" | ConvertFrom-Json)
+#$deps_import | %{ DO STH }
 
 # Links to binaries
+# TODO These you have to set manually, because the folder structure in
+# extracted files will possibly change over time
+
 $bin = @(
         [PSCustomObject]@{Name="cmake"; BinDir=""; BinPath="\bin\cmake.exe"}
         [PSCustomObject]@{Name="mingit"; BinDir=""; BinPath="\cmd\git.exe"}
         [PSCustomObject]@{Name="flex"; BinDir=""; BinPath="\win_flex.exe"}
 #        [PSCustomObject]@{Name="pip"; BinDir=""; BinPath=""}
 #        [PSCustomObject]@{Name="python3"; BinDir=""; BinPath=""}
-        [PSCustomObject]@{Name="vcpkg"; BinDir="$($dependency_dl_path)vcpkg"; BinPath="$($dependency_dl_path)vcpkg\vcpkg.exe"}
-#        [PSCustomObject]@{Name="cpack"; BinDir=""; BinPath=""\bin\cpack.exe""}
+        [PSCustomObject]@{Name="vcpkg"; BinDir=$vcpkg_path; BinPath="$($vcpkg_path)vcpkg.exe"}
+        [PSCustomObject]@{Name="cpack"; BinDir=""; BinPath="\bin\cpack.exe"}
 )
 
 
@@ -88,14 +136,16 @@ Function GenerateDepFolders{
 }
 
 # Function to download dependency setups
-# TODO Do not redownload already installed/downloaded deps
-# TODO Find a way to cache actual version number in a environment variable/text file
+# TODO Do not redownload already extracted deps
 Function DownloadDependencies($arr){
 
      Write-Host "Downloading dependencies ..."
     
      $arr | ForEach-Object {
+
+            # Alternative download command, not working with Github! 
             # Start-BitsTransfer -Source $_.HashFile -Destination $_.HomeDir -Asynchronous
+
             $source = "$($_.DownloadLink)"
             $output = "$($_.HomeDir)$($_.FileName)"
 
@@ -135,6 +185,7 @@ Function DownloadDependencies($arr){
 }
 
 # Function to generate FileName and FilePath from DownloadLink
+# TODO Add generate filename for download for 32bit/64bit
 Function GenerateFileNames($arr){
 
     Write-Host "Generating Filenames ..."
@@ -143,14 +194,14 @@ Function GenerateFileNames($arr){
            $_.FileName = "$($_.DownloadLink.SubString($_.DownloadLink.LastIndexOf('/') + 1))"
           
            # Debug
-           Write-Host $_.HomeDir
-           Write-Host $_.FileName
+           #Write-Host $_.HomeDir
+           #Write-Host $_.FileName
     }
 }
 
 # Get the link for the latest version of a github repo
 # Inspired by https://gist.github.com/f3l3gy/0e89dde158dde024959e36e915abf6bd
-# TODO But also download a hardcoded version
+# TODO Also download a hardcoded version
 # TODO $arch should be exchangable from a flag for installing just 32bit versions
 Function GetLatestVersionLink{
 	Param($arr, [string] $path)
@@ -175,7 +226,9 @@ Function GetLatestVersionLink{
                #$_.DownloadLink = ($versionRequest | Where {$_.content_type -eq "application/zip"})
            
 
-               #ADD NEW SOFTWARE LINK GENERATION HERE
+               # ADD NEW SOFTWARE LINK GENERATION HERE
+               # Move Generation of $file to new function 
+               # set up for architecture-flag
                # >>>
           
                if(($_.Name) -eq "mingit"){
@@ -206,7 +259,11 @@ Function GetLatestVersionLink{
 	       #ADD NEW SOFTWARE LINK GENERATION HERE
                # >>> elseif() <<<
 	       
-
+               # TODO
+               # Here should be 
+               # $file = GenerateFileNames-Return value or so
+               # also consider python versions not just git
+               # also consider hardcoded version file
 
                $file="$name$version$arch$type"
     
@@ -258,9 +315,9 @@ Function ExtractDependencies{
                 Write-Host "Extracting $($_.Name) files"
                 Expand-Archive -LiteralPath $zip -DestinationPath $_.HomeDir -Force
                
-                # TODO Archiv muss zum Testen nicht gelöscht werden!
-                # TODO Statt Remove-Item in Archiv-Ordner verschieben
-                # > und Version-String speichern
+                # TODO Archive doesn't need to be deleted while Testing
+                # TODO instead of Remove-Item move to Archive-Folder
+                # > and save Version string
                 # Remove-Item  $zip  -Force
 
                 $_.HomeDir = $zip.Substring(0,$zip.Length-4)
@@ -346,44 +403,60 @@ Function InstallDependencies($arr){
 # pip
 Function GetBinaryLinks($arr){
 
-      # Split env:Path
-      $split_path = $env:Path.Split(";")
-
-
       #$arr = $bin
       $arr | ForEach-Object {
                 
                # search cache
                $is_in_path=$false;
 
+               
                $name_temp = $_.Name
+               
+               # Set "cpack" as "cmake" for same directory
+               if($($_.Name) -eq "cpack"){
+                $name_temp = "cmake"
+               }
                $lookup = ($deps | Where {$($_.Name) -eq $($name_temp)})  
                $_.BinDir = $lookup.HomeDir
                $_.BinPath = "$($lookup.HomeDir)$($_.BinPath)"
                
-               $executable_dir = "$($_.BinPath.SubString(0,($_.BinPath.LastIndexOf('\') + 1)))"
-               
-               
+               # Debug for BinaryLinks
+               # Write-Host $_.BinPath
 
-               
-               # Test for the variable already being there
-               $split_path | ?{$executable_dir -eq $_} | %{ 
-                    
-                    # Debug
-                    Write-Host "$($name_temp) with $($executable_dir) is already there!" 
-                    $is_in_path = $true
-              
-               }
-
-               if(!($is_in_path)){
-                    # Debug
-                    # Write-Host "Write directory $($executable_dir) to path."
-                    Set-Item -path env:Path -value "$($env:Path);$($executable_dir)"
-               }
-    
+               # Add Directory of executable to environent $PATH variable
+               SetEnvPathFromFile -file_path $_.BinPath
+                                
      }
     
 }
+
+# Set up environment $PATH variable, if not already set
+Function SetEnvPathFromFile{
+    PARAM($file_path)
+
+     # Split env:Path
+     $split_path = $env:Path.Split(";")
+     
+     # Get Dir from File
+     $file_dir = "$($file_path.SubString(0,($file_path.LastIndexOf('\') + 1)))"
+           
+     # Test for the variable already being there
+     $split_path | ?{$file_dir -eq $_} | %{ 
+                    
+                   # Debug
+                   #Write-Host "$($file_dir) is already in env:Path!" 
+                   $is_in_path = $true
+              
+                   }
+
+    if(!($is_in_path)){
+        # Debug
+        #Write-Host "Write directory $($file_dir) to $($var)."
+        Set-Item -path env:Path -value "$($env:Path);$($file_dir)"
+    }
+
+}
+
 
 # Git imitating
 Function GitBin([string]$address,[string]$action, [string]$path){
@@ -392,18 +465,26 @@ Function GitBin([string]$address,[string]$action, [string]$path){
     $git = ($bin | Where {$($_.Name) -eq $("mingit")} | Select BinPath)
 
     GenerateFolders $path
-    Set-Location -Path $path
+    # Set-Location -Path $path
 
-    Start-Process -FilePath $git.BinPath -ArgumentList "$($action) $($address)" -Wait
+    Start-Process -FilePath $git.BinPath -ArgumentList "$($action) $($address) $($path)" -Wait
+
+    return $path
 
 }
 
 # Vcpkg imitating
-Function VcpkgBin([string]$cmd ="/help",[string]$software="", [string]$arch="--triplet x64-windows"){
+Function VcpkgBin([string]$cmd ="/help",[string]$software=""){
 
-# $vcpkg integrate install
-# $vcpkg install XXX
-    
+    # 64bit flag
+    # TODO Testing
+    if($is64bit){
+        $arch="--triplet x64-windows"
+    }else{
+        # TODO Also remove possible environment variable
+        $arch=""
+    }   
+   
     # vcpkg Binary
     $vcpkg = ($bin | Where {$($_.Name) -eq $("vcpkg")} | Select BinPath)
 
@@ -424,10 +505,7 @@ Function VcpkgBin([string]$cmd ="/help",[string]$software="", [string]$arch="--t
 }
 
 
-
 ## Main
-
-
 
 
 # Create Directory for dependency downloads
@@ -471,7 +549,7 @@ $deps | ConvertTo-Json -depth 100 | Out-File "$($dependency_dl_path)versions.jso
 GetBinaryLinks $bin
 
 # Install Python pip modules
-$pip = & Start-Process -FilePath "pip" -ArgumentList "install $($pip_modules)" -Wait
+#$pip = & Start-Process -FilePath "pip" -ArgumentList "install $($pip_modules)" -Wait
 
 
 ### Till here everything works out somewhat nicely
@@ -494,13 +572,13 @@ $pip = & Start-Process -FilePath "pip" -ArgumentList "install $($pip_modules)" -
 # Recompile latest stable
 # Do not hardcode vcpkg path in $bin-array/hashtable
 Write-Host "Please wait, while we are cloning vcpkg ..."
-GitBin -address "https://github.com/Microsoft/vcpkg.git" -action "clone" -path $dependency_dl_path
+GitBin -address "https://github.com/Microsoft/vcpkg.git" -action "clone" -path $vcpkg_path
 
 # Set-Location -Path "$($dependency_dl_path)vcpkg"
 Write-Host "Please wait, while we are compiling vcpkg ..."
 
 # DEBUG/TODO Do not rebuild everytime
-# $bat = & "$($dependency_dl_path)vcpkg\scripts\bootstrap.ps1"
+# $bat = & "$($vcpkg_path)scripts\bootstrap.ps1"
 
 # Integrating vcpkg in OS
 # VcpkgBin -cmd "integrate install"
@@ -521,7 +599,12 @@ Write-Host "Please wait, while we are compiling vcpkg ..."
 
 # Clone openage
 #Write-Host "Please wait, while we are cloning openage ..."
-#GitBin -address "https://github.com/SFTtech/openage.git" -action "clone" -path $dependency_dl_path
+$openage_src_dir=GitBin -address "https://github.com/SFTtech/openage.git" -action "clone" -path "$($dependency_dl_path)openage"
+
+
+# Saving binary paths to easily restore current status
+$bin | ConvertTo-Json -depth 100 | Out-File "$($dependency_dl_path)binaries.json"
+
 
 # Ready
 Write-Host "Here we are ready for cmake configuring and the environment should be set up."
@@ -532,10 +615,13 @@ GenerateFolders $build_dir
 Set-Location $build_dir
 
 # cmake Commands
-# cmake -DCMAKE_TOOLCHAIN_FILE=<vcpkg directory>/scripts/buildsystems/vcpkg.cmake ..
-# cmake --build . --config RelWithDebInfo -- /nologo /m /v:m
-# TODO set cmake flag -G depending on installed MSVC
-# $arch='-G "Visual Studio 15 2017 Win64"'
+$vcpkg_toolchain ="-DCMAKE_TOOLCHAIN_FILE=$($vcpkg_path)/scripts/buildsystems/vcpkg.cmake"
+$build_flag="-G $($vs_ver) $($arch)"
+
+# $job = & Start-Process -FilePath "cmake" -ArgumentList "$($vcpkg_toolchain) $($build_flag) $($openage_src_dir)" -Wait
+
+# Set-Location $build_dir
+# $job = & Start-Process -FilePath "cmake" -ArgumentList "--build . --config RelWithDebInfo -- /nologo /m /v:m" -Wait
 
 
 # Install the DejaVu Book Font.
@@ -558,6 +644,9 @@ Set-Location $build_dir
 # cpack -C RelWithDebInfo -V
 #The installer (openage-<version>-win32.exe) will be generated in the same directory.
 
+# Still needs to be downloaded -> $deps
+# NSIS
+# dejavu-fonts-ttf
 
 
 # Cleanup
